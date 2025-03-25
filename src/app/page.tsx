01d52c33ac41e50b6b59895fded1cbfe7f626684
lizlux <liz@lizlux.net>
2025-03-25 15:07:50 -0700

    broader page styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,50 +17,44 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.mastheadImage}
-          src="/Dax_Lee_Portrait_500.jpeg"
-          alt="Image of Dax Lee"
-          width={500}
-          height={333}
-          priority
-        />
-        <span className={styles.tree1}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </span>
-        <nav className={styles.nav}>
-          <ul>
-            {/* TODO: turn this into actual page navigation with routing */}
-            {/*
-              Clicking a nav link will auto-scroll the page to a specific y point
-              as described by the top y coordinate of the div with the content
-            */}
-            <li
-              className={styles.navLink}
-              onClick={() =>
-                musicElemRef.current?.scrollIntoView(scrollOptions)
-              }
-            >
-              Music
-            </li>
-            <li
-              className={styles.navLink}
-              onClick={() => techElemRef.current?.scrollIntoView(scrollOptions)}
-            >
-              Tech
-            </li>
-            <li
-              className={styles.navLink}
-              onClick={() =>
-                createElemRef.current?.scrollIntoView(scrollOptions)
-              }
-            >
-              Create
-            </li>
-          </ul>
-        </nav>
+        <div className={styles.mastheadContainer}>
+          <Image
+            className={styles.mastheadImage}
+            src="/Dax_Lee_Portrait_500.jpeg"
+            alt="Image of Dax Lee"
+            width={500}
+            height={333}
+            priority
+          />
+          <nav className={styles.nav}>
+            <ul>
+              <li
+                className={styles.navLink}
+                onClick={() =>
+                  musicElemRef.current?.scrollIntoView(scrollOptions)
+                }
+              >
+                Music
+              </li>
+              <li
+                className={styles.navLink}
+                onClick={() =>
+                  techElemRef.current?.scrollIntoView(scrollOptions)
+                }
+              >
+                Tech
+              </li>
+              <li
+                className={styles.navLink}
+                onClick={() =>
+                  createElemRef.current?.scrollIntoView(scrollOptions)
+                }
+              >
+                Create
+              </li>
+            </ul>
+          </nav>
+        </div>
         <div className={styles.featureContainer}>
           <div ref={musicElemRef} className={styles.musicContainer}>
             Music Music Music Music Music Music Music Music Music Music Music
